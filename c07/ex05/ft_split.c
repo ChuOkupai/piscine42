8ad2d1547d79bb53c2e22c13527ecfdd5b3ea31c
Adrien SOURSOU <asoursou@e2r11p15.42.fr>
2019-07-12 10:57:11 +0200

    fix c07 ex05

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -6,92 +6,80 @@
 /*   By: asoursou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 05:33:05 by asoursou          #+#    #+#             */
-/*   Updated: 2019/07/11 08:01:31 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/07/12 10:41:51 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int		ft_strlen(char *str)
-{
-	int n;
-
-	n = 0;
-	while (str[n])
-		n++;
-	return (n);
-}
-
 int		ft_is_separator(char *str, char *charset)
 {
-	if (!*charset)
-		return (0);
-	while (*charset && *str == *charset)
-	{
-		str++;
-		charset++;
-	}
-	return (*charset == '\0');
+	while (*charset)
+		if (*str == *charset++)
+			return (1);
+	return (0);
 }
 
-int		ft_wordcount(char *str, char *charset, int charset_len)
+int		ft_wordlen(char *str, char *charset)
+{
+	int i;
+
+	i = 0;
+	while (str[i] && !ft_is_separator(str + i, charset))
+		i++;
+	return (i);
+}
+
+int		ft_wordcount(char *str, char *charset)
 {
 	int i;
 	int w;
 
-	if (!*charset)
-		return (*str != '\0');
 	w = 0;
 	while (*str)
 	{
 		while (*str && ft_is_separator(str, charset))
-			str += charset_len;
-		i = 0;
-		while (str[i] && !ft_is_separator(str + i, charset))
-			i++;
+			str++;
+		i = ft_wordlen(str, charset);
+		str += i;
 		if (i)
 			w++;
-		str += i;
 	}
 	return (w);
 }
 
-char	*ft_wordcpy(char **str, char *charset, int charset_len)
+char	*ft_wordcpy(char *src, int n)
 {
-	char	*s;
-	int		i;
-	int		j;
+	char	*dest;
 
-	while (**str && ft_is_separator(*str, charset))
-		*str += charset_len;
-	i = 0;
-	while ((*str)[i] && !ft_is_separator((*str) + i, charset))
-		i++;
-	if (!(s = malloc((i + 1) * sizeof(char*))))
+	if (!(dest = malloc((n + 1) * sizeof(char))))
 		return (NULL);
-	j = i;
-	s[i] = '\0';
-	while (i-- > 0)
-		s[i] = (*str)[i];
-	*str += j;
-	return (s);
+	dest[n] = '\0';
+	while (n--)
+		dest[n] = src[n];
+	return (dest);
 }
 
 char	**ft_split(char *str, char *charset)
 {
 	char	**t;
-	int		charset_len;
 	int		size;
 	int		i;
+	int		n;
 
-	charset_len = ft_strlen(charset);
-	size = ft_wordcount(str, charset, charset_len);
+	size = ft_wordcount(str, charset);
 	if (!(t = malloc((size + 1) * sizeof(char*))))
 		return (NULL);
 	i = -1;
 	while (++i < size)
-		if (!(t[i] = ft_wordcpy(&str, charset, charset_len)))
+	{
+		while (*str && ft_is_separator(str, charset))
+			str++;
+		n = ft_wordlen(str, charset);
+		if (!(t[i] = ft_wordcpy(str, n)))
 			return (NULL);
-	t[i] = NULL;
+		str += n;
+	}
+	t[size] = 0;
 	return (t);
 }
