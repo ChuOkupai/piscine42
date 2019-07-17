77738bb8b4d09e21ff997c76aa4a015b8bcd5313
Adrien SOURSOU <asoursou@e2r11p15.42.fr>
2019-07-18 01:27:41 +0200

    fix c11

--- c11/ex04/ft_is_sort.c
+++ c11/ex04/ft_is_sort.c
@@ -6,17 +6,25 @@
 /*   By: asoursou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 03:41:29 by asoursou          #+#    #+#             */
-/*   Updated: 2019/07/17 17:36:51 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/07/18 00:10:31 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
 	int i;
+	int r;
+	int r2;
 
-	i = 0;
-	while (++i < length)
-		if (f(tab[i - 1], tab[i]) > 0)
+	i = 1;
+	while (i < length && !(r = f(tab[i - 1], tab[i])))
+		i++;
+	while (i < length)
+	{
+		r2 = f(tab[i - 1], tab[i]);
+		if ((r < 0 && r2 > 0) || (r > 0 && r2 < 0))
 			return (0);
+		i++;
+	}
 	return (1);
 }
--- c11/ex07/ft_advanced_sort_string_tab.c
+++ c11/ex07/ft_advanced_sort_string_tab.c
@@ -6,7 +6,7 @@
 /*   By: asoursou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 15:09:08 by asoursou          #+#    #+#             */
-/*   Updated: 2019/07/17 17:37:01 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/07/18 01:06:40 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,19 @@ void	ft_strswap(char **a, char **b)
 	*b = c;
 }
 
-void	ft_sort_str_tab(char **tab, int size, int (*cmp)(char *, char *))
-{
-	char	*pivot;
-	int		i;
-	int		j;
-
-	if (size < 2)
-		return ;
-	pivot = tab[--size];
-	i = 0;
-	j = -1;
-	while (++j < size)
-		if (cmp(tab[j], pivot) < 0)
-			ft_strswap(&tab[i++], &tab[j]);
-	if (cmp(tab[i], tab[size]) > 0)
-		ft_strswap(&tab[i], &tab[size]);
-	ft_sort_str_tab(tab, i, cmp);
-	ft_sort_str_tab(tab + i + 1, size - i, cmp);
-}
-
 void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
 {
-	int n;
+	int size;
+	int i;
 
-	n = 0;
-	while (tab[n])
-		n++;
-	ft_sort_str_tab(tab, n, cmp);
+	size = 0;
+	while (tab[size])
+		size++;
+	while (--size >= 0)
+	{
+		i = 0;
+		while (++i <= size)
+			if (cmp(tab[i - 1], tab[i]) > 0)
+				ft_strswap(&tab[i - 1], &tab[i]);
+	}
 }
